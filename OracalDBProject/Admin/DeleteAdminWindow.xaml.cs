b6419f95cf73b929ec116a0fde0eafc367339979
Liran Nagar <liran.nagar1993@gmail.com>
2019-02-17 00:42:29 +0200

    Liran Nagar

--- OracalDBProject/Admin/DeleteAdminWindow.xaml.cs
+++ OracalDBProject/Admin/DeleteAdminWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +23,31 @@ namespace OracalDBProject.Admin
     {
         public DeleteAdminWindow()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+                ShowAllAdmins();
+                Logger.Instance.Info("Delete Admin Window opened");
+            }
+            catch (OracleException ex)
+            {
+                Logger.Instance.Error("Erorr while trying to open Delete Admin Window \nDetails" + ex);
+            }
+        }
+
+        private void  ShowAllAdmins()
+        {
+            try
+            {
+                string searchQueryString = "SELECT ADMINISTRATOR.ADMIN_ID,USERS.FIRST_NAME,USERS.LAST_NAME,USERS.PASSWORD_ENCRYPTED,USERS.USER_EMAIL,USERS.USER_PHONE_NUMBER,ADMINISTRATOR.SALARY_NIS "
+                                                + " FROM USERS INNER JOIN ADMINISTRATOR ON USERS.USER_ID = ADMINISTRATOR.USER_ID";
+                UpdateTable(searchQueryString);
+                Logger.Instance.Info("All Admins Shown");
+            }catch(OracleException ex)
+            {
+                Logger.Instance.Error("Erorr while trying to show all admins \nDetails" + ex);
+            }
+            
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -33,6 +59,72 @@ namespace OracalDBProject.Admin
             marketsDBSourceUSERSTableAdapter.Fill(marketsDBSource.USERS);
             System.Windows.Data.CollectionViewSource uSERSViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("uSERSViewSource")));
             uSERSViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void searchButtonDeleteWindow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string searchQueryString = "";
+                string select = searchComboBoxDeleteAdmin.SelectedItem.ToString();
+                if (select.Contains("By ID"))
+                {
+                    searchQueryString = "SELECT ADMINISTRATOR.ADMIN_ID,USERS.FIRST_NAME,USERS.LAST_NAME,USERS.PASSWORD_ENCRYPTED,USERS.USER_EMAIL,USERS.USER_PHONE_NUMBER,ADMINISTRATOR.SALARY_NIS "
+                                + " FROM USERS INNER JOIN ADMINISTRATOR ON USERS.USER_ID = ADMINISTRATOR.USER_ID"
+                                + " WHERE ADMINISTRATOR.USER_ID= " + textBoxSearchDeleteAdmin.Text + "";
+                }
+                else
+                {
+                    searchQueryString = "SELECT ADMINISTRATOR.ADMIN_ID,USERS.FIRST_NAME,USERS.LAST_NAME,USERS.PASSWORD_ENCRYPTED,USERS.USER_EMAIL,USERS.USER_PHONE_NUMBER,ADMINISTRATOR.SALARY_NIS "
+                                + " FROM USERS INNER JOIN ADMINISTRATOR ON USERS.USER_ID = ADMINISTRATOR.USER_ID"
+                                + " WHERE USERS.FIRST_NAME  = '" + textBoxSearchDeleteAdmin.Text + "'";
+                }
+                UpdateTable(searchQueryString);
+            }
+            catch (OracleException ex)
+            {
+                Logger.Instance.Error("Exception while trying to search to Delete Product\nDeatails: " + ex);
+            }
+        }
+
+        private void UpdateTable(string query)
+        {
+            try
+            {
+                OracleSingletonComment.Instance.CommandText = query;
+                OracleSingletonComment.Instance.ExecuteNonQuery();
+                OracleDataAdapter da = new OracleDataAdapter(OracleSingletonComment.Instance);
+                da.SelectCommand = OracleSingletonComment.Instance;
+                DataTable dt = new DataTable("USERS");
+                da.Fill(dt);
+                dataGrid.ItemsSource = dt.DefaultView;
+                Logger.Instance.Info("Table Updated");
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show("Wrong Value!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Logger.Instance.Error("Exception while trying to update table\nDeatails: " + ex);
+            }
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AdminPanel adminPanel = new AdminPanel();
+                adminPanel.Show();
+                this.Close();
+                Logger.Instance.Info("Open Admin Paenel Window and Close Delete Admin Window");
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.Error("Exception while trying to open admin panel window and Close Delete Admin Window \nDetails: " + ex);
+            }
+        }
+
+        private void showAllAdminsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowAllAdmins();
         }
     }
 }
